0cc176316d37d1498a8991312d77248b1fd42d84
ThatcherK <kaliithatmau@gmail.com>
2020-06-24 14:55:41 +0300

    mid stream commit

--- config.py
+++ config.py
@@ -4,24 +4,24 @@ import os
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     BCRYPT_LOG_ROUNDS = 12
     TOKEN_EXPIRATION_DAYS = 5
     TOKEN_EXPIRATION_SECONDS = 0
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://thatcher:post@localhost:5432/greenmile_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL") or "postgresql://thatcher:post@localhost:5432/greenmile_test_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL")
     SECRET_KEY = "testkey"
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://thatcher:post@localhost:5432/greenmile_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 config = {
